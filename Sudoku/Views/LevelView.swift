e6e3d8d14eb20414bc86f6b45a4a23bc7cdc2819
Jisu Lim <limjisu@gmail.com>
2025-07-15 13:02:49 -0700

    feat: added LevelView

---
+++ Sudoku/Views/LevelView.swift
@@ -0,0 +1,24 @@
+//
+//  LevelView.swift
+//  Sudoku
+//
+//  Created by Jisu Lim on 7/15/25.
+//
+
+import SwiftUI
+
+/// - Type: Action Sheet
+/// - 4개의 Button을 갖는다
+///     - Easy
+///     - Medium
+///     - Hard
+///     - (Resume)
+struct LevelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LevelView()
+}
