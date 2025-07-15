0f2b6fd6751857cfce00c736a24472826787c5a0
Jisu Lim <limjisu@gmail.com>
2025-07-15 13:00:21 -0700

    feat: Added SettingView and changed iOS deployment target to 18.0

--- Sudoku/Views/SettingView.swift
+++ Sudoku/Views/SettingView.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+/// - Type: Action Sheet
+/// - 1개의 Button을 갖는다
+///     - Tile: Home
+///     - Action: GameHomeView 열기 또는 dismiss()
+
 struct SettingView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
