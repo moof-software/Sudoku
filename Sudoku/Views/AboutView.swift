e98cb7381a324cf6409b5c6edffab228c19768f6
Jisu Lim <limjisu@gmail.com>
2025-07-15 12:44:33 -0700

    chore: removed Support page

--- Sudoku/Views/AboutView.swift
+++ Sudoku/Views/AboutView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 ///
 /// - 1개의 NavigationBarItem을 갖는다
 ///     -- Tile: Home
-///     -- Action: GameHomeView 열기
+///     --  Action: GameHomeView 열기
 struct AboutView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
