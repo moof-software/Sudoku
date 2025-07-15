514e1ba69fd82ecc18b3bc2cce03b4bed1e40535
Jisu Lim <limjisu@gmail.com>
2025-07-15 12:59:08 -0700

    feat: added SettingView and changed iOS version to 18.0

---
+++ Sudoku/Views/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  Sudoku
+//
+//  Created by Jisu Lim on 7/15/25.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
