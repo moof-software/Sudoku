cc45a58c02dfe76641ede907a192a6d9af138d5f
Jisu Lim <limjisu@gmail.com>
2025-07-15 13:19:23 -0700

    feat: added SolutionView

---
+++ Sudoku/Views/SolutionView.swift
@@ -0,0 +1,22 @@
+//
+//  SolutionView.swift
+//  Sudoku
+//
+//  Created by Jisu Lim on 7/15/25.
+//
+
+import SwiftUI
+
+/// - Type: Navigation Stack
+/// - (임시) 1개의 button을 갖는다
+///     - Tile: Home
+///     - Action: GameHomeView 열기 또는 dismiss()
+struct SolutionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SolutionView()
+}
