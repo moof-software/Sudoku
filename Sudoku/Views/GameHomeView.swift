d63ec7e1a870d3899d52b839fc5a34b53458593f
imoofs <imoofs@gmail.com>
2025-07-16 11:45:38 -0700

    chore: clean up and eliminate 'SwiftData' related terms 2

--- Sudoku/Views/GameHomeView.swift
+++ Sudoku/Views/GameHomeView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import SwiftData
 
 /// 6개의 버튼이 필요하다.
 /// - About - AboutView(NavigationStack type) 열기
@@ -25,53 +24,13 @@ import SwiftData
 ///     - a: The *a* component of the vector.
 ///
 struct GameHomeView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
+        Text("Sudoku Home View")
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
 }
 
 #Preview {
     GameHomeView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
