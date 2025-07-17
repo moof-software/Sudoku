4b9332d1276be13284a77c3926c4f460f419983d
imoofs <imoofs@gmail.com>
2025-07-17 08:37:36 -0700

    feat: add StartUpView and its controller at SudokuApp

---
+++ Sudoku/Views/StartUp/StartUpView.swift
@@ -0,0 +1,18 @@
+//
+//  StartUpView.swift
+//  Sudoku
+//
+//  Created by Hungu Lim on 7/16/25.
+//
+
+import SwiftUI
+
+struct StartUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartUpView()
+}
