b2ec7413b8c4b2c105ed5d6e85f1883ff05dfdcb
Jisu Lim <limjisu@gmail.com>
2025-07-20 10:23:10 -0700

    feat: added all the text and buttons for LevelView

--- Sudoku/Views/LevelView.swift
+++ Sudoku/Views/LevelView.swift
@@ -15,7 +15,38 @@ import SwiftUI
 ///     - (Resume)
 struct LevelView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button("< Back") {
+                    // Connect to GameHomeView
+                }
+                .padding()
+                Spacer()
+            }
+            Text("Levels")
+                .font(.largeTitle)
+                .bold()
+            Spacer()
+            
+            Button("Easy") {
+                // Change "score" text in GameBoard View to easy
+            }
+            .buttonStyle(.bordered)
+            
+            Button("Medium") {
+                // Change "score" text in GameBoard View to medium
+            }
+            .buttonStyle(.bordered)
+            
+            Button("Hard") {
+                // Change "score" text in GameBoard View to hard
+            }
+            .buttonStyle(.bordered)
+            Spacer()
+            Button("Resume") {
+                // Change "score" text in GameBoard View to resume
+            }
+        }
     }
 }
 
