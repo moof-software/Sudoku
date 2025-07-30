80f3ec274acb0bd57a7735bd98d2cda1cd8d665a
moof <imoofs@gmail.com>
2025-07-30 10:51:16 +0900

    feat: Configured first view, GameHomeView, with two types button view.

---
+++ Sudoku/Views/Components/IconNameButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  IconNameButtonView.swift
+//  Sudoku
+//
+//  Created by Hungu Lim on 7/30/25.
+//
+
+import SwiftUI
+
+struct IconNameButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconNameButtonView()
+}
