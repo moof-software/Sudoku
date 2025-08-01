a0c11ba157c76d84b8dbb4b6b79d2caf992fc6cb
waterkimchi <hyunsul2@uci.edu>
2025-08-02 08:44:08 +0900

    feat: added HelpView

--- Sudoku/Views/HelpView.swift
+++ Sudoku/Views/HelpView.swift
@@ -8,26 +8,39 @@
 import SwiftUI
 
 /// View that shows a Sudoku tutorial for first-time users.
-///
-/// Contains:
-/// - VStack
-///     - HStack
-///         - Back Button
-///             - Label: "< Back"
-///             - Action: Connect to GameHomeView
-///         - Next Button
-///             - Label: "Next >"
-///             - Action: Change `displayedText` to "Next Page"
-///     - `displayedText` Text
-///         - Default must be "Help Screen"
+/// - Scrollview (horizontal, width 343 height 635)
+///    - HStack
+///        - Images
+/// - Modifier: scrollTargetBehavior(viewAligned)
+
 struct HelpView: View {
+    @State private var scrolledID: Int? = 0
+    private var images: [Image] = [
+        Image("HelpView"),
+        Image("HelpView"),
+        Image("HelpView"),
+        Image("HelpView")
+    ]
 
     var body: some View {
         VStack {
-            Spacer()
-            // Help Screen
-            Text("Help Screen")
-            Spacer()
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 0) {
+                    ForEach(0..<images.count, id: \.self) { index in
+                        images[index]
+                            .resizable()
+                            .scaledToFit()
+                            .containerRelativeFrame(.horizontal, count: 1, span: 1, spacing: 0, alignment: .center)
+                    }
+                }
+                .scrollTargetLayout()
+            }
+            .scrollTargetBehavior(.paging)
+            .scrollPosition(id: $scrolledID)
+            .scrollIndicators(.never)
+
+            Text("\(scrolledID ?? 0)/\(images.count - 1)")
+                .padding()
         }
     }
 }
