53cd109c1b619875628dc9af80c0d424ad8666d4
moof <imoofs@gmail.com>
2025-08-21 10:39:25 +0900

    feat: Consider Cell's Visible status for Board and Cell note

--- Sudoku/Model/BoardNotes.swift
+++ Sudoku/Model/BoardNotes.swift
@@ -18,11 +18,21 @@ class BoardNotes: Codable {
         self.block = block
     }
 
-    func updateNotes(position: GridInfo, value: Int) {
-        row[position.row].insert(value)
-        col[position.col].insert(value)
-        block[(position.row / 3) * 3 + (position.col / 3)].insert(
-            value
-        )
+    func updateNotes(data: SudokuCell) {
+        let position = data.position.board
+
+        if data.visible {
+            row[position.row].remove(data.value)
+            col[position.col].remove(data.value)
+            block[(position.row / 3) * 3 + (position.col / 3)].remove(
+                data.value
+            )
+        } else {
+            row[position.row].insert(data.value)
+            col[position.col].insert(data.value)
+            block[(position.row / 3) * 3 + (position.col / 3)].insert(
+                data.value
+            )
+        }
     }
 }
