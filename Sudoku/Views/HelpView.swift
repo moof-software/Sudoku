b838d297d330f5707c0cedee343a8fe27806d826
bknging <limjisu@gmail.com>
2025-09-04 17:00:57 -0700

    feat: remodeled HelpView with TabView instead of ScrollView, added ads to other views

--- Sudoku/Views/HelpView.swift
+++ Sudoku/Views/HelpView.swift
@@ -30,34 +30,24 @@ struct HelpView: View {
     ]
 
     var body: some View {
-        VStack {
-            ScrollView(.horizontal) {
-                LazyHStack(spacing: 0) {
-//                - Images put in using ForEach
-//                    - Modifiers: resizeable, scaled to fit,
-//                    container relative frame adjusted for horizontal,
-//                    count and span 1.
-                    ForEach(0..<images.count, id: \.self) { index in
-                        images[index]
-                            .resizable()
-                            .scaledToFit()
-                            .containerRelativeFrame(.horizontal, count: 1, span: 1, spacing: 0, alignment: .center)
-                    }
+        TabView {
+            ForEach(0..<images.count, id: \.self) { index in
+                Tab("\(index + 1)", systemImage: "\(index + 1).circle") {
+                    images[index]
+                        .resizable()
+                        .scaledToFit()
+                        .containerRelativeFrame(
+                            .horizontal,
+                            count: 1,
+                            span: 1,
+                            spacing: 0,
+                            alignment: .center
+                        )
                 }
-//            - Modifier: Scroll to Target Layout
-                .scrollTargetLayout()
             }
-//        - Modifiers: Scroll to Target Behavior(paging),
-//            Scroll Position(with ID),
-//            Scroll Indicators (set to never)
-            .scrollTargetBehavior(.paging)
-            .scrollPosition(id: $scrolledID)
-            .scrollIndicators(.never)
-
-//        - Text that shows page/all page number
-            Text("\(scrolledID ?? 0)/\(images.count - 1)")
-                .padding()
         }
+        .tabViewStyle(PageTabViewStyle())
+        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
     }
 }
 
