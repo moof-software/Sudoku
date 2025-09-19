893ea0b11b46c928f54806d7278d948ecdadd377
imoofs <31395902+imoofs@users.noreply.github.com>
2025-09-19 11:12:11 +0900

    Update CellView.swift

--- Sudoku/Views/Board/CellView.swift
+++ Sudoku/Views/Board/CellView.swift
@@ -72,8 +72,7 @@ struct CellView: View {
                     if sudoku.table[row][col].position.board.row
                         == selectedCell.row
                         || sudoku.table[row][col].position.board.col
-                            == selectedCell.col
-                    {
+                            == selectedCell.col {
                         RoundedRectangle(cornerRadius: 4)
                             .foregroundStyle(Color.blue.opacity(0.1))
                             .allowsHitTesting(false)
