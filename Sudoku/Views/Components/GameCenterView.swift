a04fb2ed52d58c9a35ae3dcaecd3a029fd1844e2
moof <imoofs@gmail.com>
2025-09-26 12:22:23 +0900

    feat: Implement SettingView

---
+++ Sudoku/Views/Components/GameCenterView.swift
@@ -0,0 +1,18 @@
+//
+//  GameCenterView.swift
+//  Sudoku
+//
+//  Created by Hungu Lim on 9/26/25.
+//
+
+import SwiftUI
+
+struct GameCenterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameCenterView()
+}
